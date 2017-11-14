0fc2d45350fe3f1f6434ce566a56c8888a328c91
Sarah Schaller <sarcsch@aol.com>
2017-11-14 20:36:49 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
-  
+  validates :name, presence: true
+  validates :content, presence: true
+
+  belongs_to :user
+  has_many :tags, through: :post_tags
+  has_many :post_tags
+
 end
